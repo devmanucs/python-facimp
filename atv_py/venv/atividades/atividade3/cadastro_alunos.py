f00e2398f727c7d5b5822253dad44ae264a3a23f
Manuella <manuhcsantos@gmail.com>
2024-09-13 00:47:11 -0300

    atualização
    att

--- atv_py/venv/atividades/atividade3/cadastro_alunos.py
+++ atv_py/venv/atividades/atividade3/cadastro_alunos.py
@@ -1,26 +1,30 @@
 class Aluno:
-    def __init__(self, nome, nota1, nota2):
-        self.nome = nome
-        self.nota1 = nota1
-        self.nota2 = nota2
-    def media_aluno (self):
-        return(self.__nota1 + self.__nota2) / 2 
+    def __init__(self, nome: str, nota1: float, nota2: float):
+        self.__nome = nome
+        self.__nota1 = nota1
+        self.__nota2 = nota2
+
+    def get_nome (self):
+        return self.__nome
     
-    def aluno_novo (nome, nota1, nota2):
-        aluno_novo = Aluno (nome, nota1, nota2)
-        cadastro_aluno.append(aluno_novo)
-
-    def ler_dados ():
-        nome = (input('Qual seu nome? '))
-        nota1 = float(input ('Qual sua primeira nota? '))
-        nota2 = float(input ('Qual a sua segunda nota? '))
-        return nome, nota1, nota2
+    def get_nota1 (self):
+        return self.__nota1
     
-    def exibir_info (self):
-        media = self.media_aluno()
-        print(f'Seu nome é: {self.nome} e sua média é: {media: .2f}')
+    def get_nota2 (self):
+        return self.__nota2
+    
+    def get_media (self):
+        return (self.__nota1 + self.__nota2) /2
 
-    nome, nota1, nota2 = ler_dados()
-    aluno_novo(nome, nota1, nota2)
-
+def aluno_novo ():
+    nome = input('Qual seu nome? ')
+    nota1 = float(input('Qual sua primeira nota? '))
+    nota2 = float(input('Qual sua segunda nota? '))
+    return Aluno (nome, nota1, nota2)
+    
+Aluno = aluno_novo()
+print (f'Nome: {Aluno. get_nome ()}')
+print (f'Primeira nota : {Aluno. get_nota1 ()}')
+print (f'Segunda nota : {Aluno. get_nota2 ()}')
+print(f'A média é: {Aluno. get_media ()} ')
 
