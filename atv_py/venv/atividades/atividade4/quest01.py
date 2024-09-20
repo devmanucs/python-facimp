0a1cb890cf7683e3b39162916bb7fc00788967b5
Manuella <manuhcsantos@gmail.com>
2024-09-19 22:44:47 -0300

    update2

--- atv_py/venv/atividades/atividade4/quest01.py
+++ atv_py/venv/atividades/atividade4/quest01.py
@@ -1,7 +1,7 @@
 #exercício de sala 13/09/2024. Instruções:​ Crie uma classe base chamada "Empregado" que contenha atributos gerais como nome e salario_base. ​Crie subclasses como "Gerente" e "Vendedor", que herdam de "Empregado", com suas próprias regras de cálculo de salário (por exemplo, o gerente tem um bônus fixo, enquanto o vendedor tem uma comissão sob as vendas).
 
 class Empregado:
-    def _init_(self, nome, salario_base:float):
+    def __init__(self, nome, salario_base:float):
         self.nome = nome
         self.salario_base = salario_base
 
